23feea40f27163be87fe6fbd6826b1868acf6e90
keiki-saito <keicake0808@gmail.com>
2021-01-03 22:36:56 +0900

    learn associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_092438) do
+ActiveRecord::Schema.define(version: 2021_01_03_124338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,10 +46,15 @@ ActiveRecord::Schema.define(version: 2021_01_03_092438) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: falseare
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
